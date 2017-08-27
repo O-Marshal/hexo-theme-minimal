e92ca50b99308573a73fcb9f9e0bd66cd9bbefb5
Ckryo <ckryo@CkryodeMacBook-Pro.local>
2017-08-27 21:45:50 +0800

    -

--- scripts/generatorPostIndex.js
+++ scripts/generatorPostIndex.js
@@ -1,7 +1,7 @@
 var pagination = require('hexo-pagination');
 
 hexo.extend.generator.register('post_index', function(locals) {
-    return pagination('/post', locals.posts, {
+    return pagination('/post', locals.posts.sort('date', -1), {
         format: 'page/%d/',
         layout: 'post_index'
     });
